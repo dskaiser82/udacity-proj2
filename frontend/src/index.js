3ea2bce0dbf7a7982392c6ae6f88707274c7f6c8
Dan Kaiser <dskaiser82@gmail.com>
2018-04-26 20:46:17 -0700

    imprting from redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose} from 'redux'; //for Redux
+
+
 import ReadApp from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
